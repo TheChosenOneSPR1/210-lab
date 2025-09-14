4abb9657913267bdc520d8234dd5d83627d51840
TheChosenOneSPR1 <rijallprasadshivam@gmail.com>
2025-09-14 13:06:22 -0700

    Lab 7.cpp

---
+++ Lab 7.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int SIZE = 5;
+
+void displayArray(string *arr) {
+    for (int i = 0; i < SIZE; i++) {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
+}
+
+string* reverseArray(string *arr) {
+    for (int i = 0; i < SIZE / 2; i++) {
+        string temp = *(arr + i);
+        *(arr + i) = *(arr + (SIZE - 1 - i));
+        *(arr + (SIZE - 1 - i)) = temp;
+    }
+    return arr;
+}
+
+int main() {
+    string *names = new string[SIZE];
+
+    *(names + 0) = "Janet";
+    *(names + 1) = "Jeffe";
+    *(names + 2) = "Jin";
+    *(names + 3) = "Joe";
+    *(names + 4) = "Junio";
+
+    cout << "Original array: ";
+    displayArray(names);
+
+    reverseArray(names);
+
+    cout << "Reversed array: ";
+    displayArray(names);
+
+    delete[] names;
+    return 0;
+}
